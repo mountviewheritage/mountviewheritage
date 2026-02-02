bb4a7b7c74ba51d5be0dfee756228dee61a0bb8d
shiv-ka-ansh <shivanshg2002@gmail.com>
2026-02-02 18:42:13 +0530

    dlt

--- js/include-bottom-bar.js
+++
@@ -1,9 +0,0 @@
-// Load Mobile Bottom Bar on every page
-fetch("bottom-bar.html")
-  .then(res => res.text())
-  .then(data => {
-    const container = document.getElementById("bottom-bar");
-    if (container) {
-      container.innerHTML = data;
-    }
-  });
--- js/include-footer.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    fetch("footer.html")
-        .then(res => res.text())
-        .then(data => {
-            document.getElementById("footer-container").innerHTML = data;
-        });
-});
